540ea98d004f8cae48bd4259517b519c2526c61b
Deyvis17 <deyvisjaime17@gmail.com>
2021-11-04 11:57:08 -0500

    Updates

---
+++ precache-manifest.d44b0368ebcc4848df1ffca3b8feca7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2bf62ff40a7488ee16274ff1e4e2b00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bde6b6f148e1b1cf0ab3",
+    "url": "/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "e8ce2023c652ee682561",
+    "url": "/static/js/2.6d022438.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/static/js/2.6d022438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde6b6f148e1b1cf0ab3",
+    "url": "/static/js/main.8188e23b.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/unichat-react-firebase/precache-manifest.5dbaf05fcca368720fb7c6e0ffe685ac.js"
+  "/precache-manifest.d44b0368ebcc4848df1ffca3b8feca7c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/unichat-react-firebase/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
